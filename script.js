8de9756275f8a4e9dfd4e861aac5d865a5378d0f
SLangrick <slangrick@hotmail.co.uk>
2020-10-23 16:45:43 +0100

    Update script.js
    Setting Sustain - length of note

--- script.js
+++ script.js
@@ -37,17 +37,29 @@ function initColorPicker() {
         let r = getNote(numHex(rgba[0]).charAt(1))
         let g = getNote(numHex(rgba[1]).charAt(1))
         let b = getNote(numHex(rgba[2]).charAt(1))
-        Chord(r,g,b)
+        let sus = getSus(numHex(rgba[0]).charAt(0))
+        let vol = getVol(numHex(rgba[1]).charAt(0))
+        Chord(r,g,b, sus)
         console.log(r[0] + g[0] + b[0])
         console.log("color:" + color)
     }
-  }
+}
     function numHex(s){
         var a = s.toString(16);
         if ((a.length % 2) > 0) {
             a = "0" + a;
         }
         return a;
+    }
+    function getSus(hex){
+        num = parseInt(hex, 16)
+        num = num / 2
+        return num
+    }
+    function getVol(hex){
+        num = parseInt(hex, 16)
+        num = num / 16
+        return num
     }
 
     function getNote(hex){
@@ -91,12 +103,12 @@ function initColorPicker() {
         }
     }
 
-    function Chord(r,g,b){
-        let piano = Synth.createInstrument('piano');
-        
-        piano.play(r[0], 2 + r[1], 2);
-        piano.play(g[0], 3 + g[1], 2);
-        piano.play(b[0], 4 + b[1], 2);
+    function Chord(r,g,b, sus, vol){
+        let piano = Synth.createInstrument('piano')
+        //Synth.setVolume(vol)
+        piano.play(r[0], 2 + r[1], sus);
+        piano.play(g[0], 3 + g[1], sus);
+        piano.play(b[0], 4 + b[1], sus);
     }
   
   initColorPicker()
